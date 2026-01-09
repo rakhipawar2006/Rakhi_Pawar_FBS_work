276304e6cba38675c99d04c6c19298874aeec337
rakhipawar2006 <rakhipawar293@gmail.com>
2026-01-09 00:52:13 -0800

    Delete Test directory

--- Test/Test 1/Q2.py
+++
@@ -1,7 +0,0 @@
-P = int(input("Enter the Principal amount : "))
-R = int(input("Enter thr Rate : "))
-T = int(input("Enter the Time period (years) : "))
-
-SI = (P * R * T) / 100
-
-print("Simple Interest : ",SI)
