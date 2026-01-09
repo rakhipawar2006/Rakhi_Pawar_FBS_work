fd7d863e02d4e2f3b3c118294c2d9bdaa07cb27d
rakhipawar2006 <rakhipawar293@gmail.com>
2026-01-09 00:50:54 -0800

    Add files via upload

---
+++ Python_Test/Test 1/Q2.py
@@ -0,0 +1,7 @@
+P = int(input("Enter the Principal amount : "))
+R = int(input("Enter thr Rate : "))
+T = int(input("Enter the Time period (years) : "))
+
+SI = (P * R * T) / 100
+
+print("Simple Interest : ",SI)
---
+++ Python_Test/Test 1/Q3.py
@@ -0,0 +1,10 @@
+dist = float(input("Enter the distance in Kilometer : "))
+
+# 1km = 1000m
+meters = dist * 1000
+print(f"Distance {dist}km in Meters is {meters}m.")
+
+# 1m = 100cm
+#1km = 1000m = 100000cm
+cm = dist * 100000
+print(f"Distance {dist}km in Centimeters is {cm}cm.")
